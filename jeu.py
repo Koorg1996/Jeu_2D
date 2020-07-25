e3518ff0c8d6ce2d1dae06ee03e3c97090bcdd5f
Koorg1996 <60946701+Koorg1996@users.noreply.github.com>
2020-07-25 15:55:18 +0200

    Update and rename main.py to jeu.py

--- jeu.py
+++ jeu.py
@@ -66,7 +66,3 @@ class jeu():
             self.carte.affichage()
         print("Bravo")
 
-df = pd.DataFrame([["x","x","x","x","x"], ["x", "*","","o","x"],["x","x","x","","x"], ["x", "","","","*","x"]])
-test = carte(df)
-game = jeu(test)
-game._main_()
