4500733ed4b07e8476a239c29ad1bf78c73cada6
Koorg1996 <60946701+Koorg1996@users.noreply.github.com>
2020-07-26 16:21:17 +0200

    Create plans.py

---
+++ plans.py
@@ -0,0 +1,4 @@
+class plans():
+    def __init__(self):
+        self.plans = [pd.DataFrame([["x","x","x","x","x", "x"], ["x", "*","","o","x", "x"],["x","x","x","b","x", "x"], ["x", "","","","*","x"],["x", "x","x","","x","x"]])
+                      ]
