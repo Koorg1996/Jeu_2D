6c66c8325d594619445893f41806bd954a7cc9fd
Koorg1996 <60946701+Koorg1996@users.noreply.github.com>
2020-07-26 16:21:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import jeu
 
-df = pd.DataFrame([["x","x","x","x","x"], ["x", "*","","o","x"],["x","x","x","","x"], ["x", "","","","*","x"]])
-test = carte(df)
-game = jeu(test)
+game = jeu()
 game._main_()
+
