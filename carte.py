27038ffa16f24db5b9e5e778cfec6771e5ebd39e
Koorg1996 <60946701+Koorg1996@users.noreply.github.com>
2020-07-26 16:20:42 +0200

    Update carte.py

--- carte.py
+++ carte.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 import joueur
 import objectif
@@ -9,12 +8,13 @@ import vide
 
      
 class carte():
-    def __init__(self, plan):
-        self.shape = list(plan.shape)
+    def __init__(self, numero):
+        self.plan = plans().plans[numero-1]
+        self.shape = list(self.plan.shape)
         self.nbr_murs = 0
         self.nbr_objectifs = 0
-        self.plan = plan.applymap(self.icone_to_classe)
-        self.trouver_joueur()
+        self.nbr_boites = 0
+        self.icone_to_classe()
         
     def classe_to_icone(objet):
         return objet.icone
@@ -32,17 +32,34 @@ class carte():
                     continue
         return
         
-    def icone_to_classe(self, objet):
-        if objet == "x":
-            self.nbr_murs += 1
-            return mur()
-        elif objet == "o":
-            return joueur()
-        elif objet == "*":
-            self.nbr_objectifs += 1
-            return objectif()
-        else:
-            return vide()
+    def icone_to_classe(self):
+        for i in range(self.shape[0]):
+            for j in range(self.shape[1]):
+                endroit = self.plan.iloc[i,j]
+                if endroit == "x":
+                    self.nbr_murs += 1
+                    m = mur()
+                    m.location = [i,j]
+                    self.plan.iloc[i,j] =  m
+                elif endroit == "o":
+                    jo = joueur()
+                    jo.location = [i,j]
+                    self.plan.iloc[i,j] = jo
+                    self.joueur = jo
+                elif endroit == "*":
+                    self.nbr_objectifs += 1
+                    ob = objectif()
+                    ob.location = [i,j]
+                    self.plan.iloc[i,j] = ob
+                elif endroit == "b":
+                    self.nbr_boites += 1
+                    b = boite()
+                    b.location = [i,j]
+                    self.plan.iloc[i,j] = b
+                else:
+                    v = vide()
+                    v.location = [i,j]
+                    self.plan.iloc[i,j] = v
     
     def still_inside(self, i, j):
         if i >= 0 and i < self.shape[0] and j >= 0 and j < self.shape[1]:
@@ -66,6 +83,9 @@ class carte():
     def what_is_there(self,i,j):
         return type(self.plan.iloc[i,j])
     
+    def what_object_is_there(self,i,j):
+        return self.plan.iloc[i,j]
+    
     def deplacement(self, i, j, i_2, j_2):
         if self.still_inside(i_2, j_2):
             obj = self.get_(i,j)
@@ -73,14 +93,3 @@ class carte():
             return
         else:
             return
-    
-    def trouver_joueur(self):
-        for i in range(self.shape[0]):
-            for j in range(self.shape[1]):
-                if type(self.plan.iloc[i,j]) == joueur:
-                    self.joueur = self.plan.iloc[i,j]
-                    self.joueur.location = [i,j]
-                    return
-                else:
-                    continue
-        return 
